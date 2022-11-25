ebb85e3adf1b8c21015be74108308ad1012c6c23
Sakibhasan <hasansakib047@gmai.,com>
2022-11-25 17:07:36 +0600

    ok

--- src/pages/Home/ProductCatagoriesData/ProductCatagoriesData.js
+++ src/pages/Home/ProductCatagoriesData/ProductCatagoriesData.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProductCatagoriesData = ({ catagory }) => {
-  const { _id,img,name } = catagory;
-  console.log(_id, img,name)
+  const { _id,img,CatagoryName } = catagory;
+  console.log(_id, img,CatagoryName)
   return (
     <div className="border-slate-400 rounded-md border-2 px-4 pb-9">
      
-      <p className="text-xl text-green-800 font-bold mt-2 text-center">{name}</p>
+      <p className="text-xl text-green-800 font-bold mt-2 text-center">{CatagoryName}</p>
       <img className="mt-3 h-3/5 mx-auto w-full mb-7" src={img} alt="" />
       <Link to={`/catagory`}>
         <button className="block w-9/12 rounded-md bg-red-500 mx-auto  mt-2 h-10 text-white font-bold text-xl">
